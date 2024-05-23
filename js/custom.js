bb69b5ada02369ec286f463b75649b9e619fbc38
hnin <hnin0929@ecplaza.net>
2024-05-23 17:51:17 +0900

    make header responsive

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+window.onload = function(){
+    const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+  
+  toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
+  }
