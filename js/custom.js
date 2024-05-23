99acc60cce31b8362a0be7b6b889145a4d57c135
hnin <hnin0929@ecplaza.net>
2024-05-23 14:54:33 +0900

    add header style

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownItems = document.querySelectorAll('.dropdown-content a');
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            const selectedLang = this.getAttribute('data-lang');
+            dropdownBtn.textContent = selectedLang;
+        });
+    });
+});
